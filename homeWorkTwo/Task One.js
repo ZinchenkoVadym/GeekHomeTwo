9ee7457d07c8d45509c3b7bc8bd137af732d149b
Vadym <73366100+ZinchenkoVadym@users.noreply.github.com>
2020-11-19 22:16:59 +0200

    Delete Task One.js

--- homeWorkTwo/Task One.js
+++
@@ -1,9 +0,0 @@
-//1. Task one
-function customCycle(result, numberOfIterations) {
-    if (numberOfIterations === 1) {
-        return result;
-    } else {
-        let finalResult = result * customCycle(result, numberOfIterations - 1);
-        return finalResult;
-    }
-}
